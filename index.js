b8d1eadcff535a0fc98371330e82070929c50356
Joel Estrada <joeljazzhands@Joels-MBP.attlocal.net>
2023-05-17 08:10:01 -0700

    added test for the shapes and added a svg constructor for in the index file

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
+const {CIrlce, Square, Triangle} = require("./lib/shape")
+
+class Svg{
+    constructor(){
+        this.shapeElement = ''
+        this.textElement = ''
+    }
+    render(){
+        return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text,color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    } 
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+    }
+}
 
 const questions = [
     {
